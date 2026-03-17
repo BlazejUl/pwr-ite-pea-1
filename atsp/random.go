15f9b2a13d7e3c7bf884592a788c5be68674ad36
blazej <blazej.uliasz2004@gmail.com>
2026-03-17 15:25:59 +0100

    made random

--- atsp/random.go
+++ atsp/random.go
@@ -1,7 +1,7 @@
 package atsp
 
 import (
-	"fmt"
+	"math/rand"
 
 	"github.com/BlazejUl/pwr-ite-pea-1/graph"
 )
@@ -18,19 +18,52 @@ func NewRandom(G graph.Graph) *Random {
 	return &Random{graph: G}
 }
 
-func (ra *Random) Solve(startVertex int) (int, []int) {
-	visited := make([]bool, ra.graph.GetVerticesNum())
+// Funkcja sprawdzająca wylosowane rozwiązania i zapisująca najlepszy
+func (ra *Random) Solve() (int, []int) {
 	path := make([]int, 0, ra.graph.GetVerticesNum())
 	bestPath := make([]int, ra.graph.GetVerticesNum())
 	bestCost := 2147483644
-	//cost := 0
+	cost := 0
 
-	visited[startVertex] = true
-	path = append(path, startVertex)
 	for range 10 * ra.graph.GetVerticesNum() {
-		fmt.Println("l")
+		cost, path = ra.getRandom()
+		if bestCost > cost {
+			bestCost = cost
+			copy(bestPath, path)
+		}
 	}
-	//cost, path = ra.BFRec(startVertex)
 
 	return bestCost, bestPath
+}
+
+// Funkcja losowo wyznaczająca drogę
+func (ra *Random) getRandom() (int, []int) {
+	path := make([]int, 0, ra.graph.GetVerticesNum())
+	visited := make([]bool, ra.graph.GetVerticesNum())
+	visited[0] = true
+	cost := 0
+	currentVertex := 0
+
+	for i := 0; i < ra.graph.GetVerticesNum(); i++ {
+		r := rand.Intn(ra.graph.GetVerticesNum())
+		// jeżeli dane misto zostało już odwiedzone to przechodzi po kolei aż natrafi na poprawne
+		if visited[r] {
+			for {
+				r = (r + 1) % ra.graph.GetVerticesNum()
+				if !visited[r] {
+					break
+				}
+			}
+		}
+		visited[r] = true
+		path = append(path, r)
+		cst, _ := ra.graph.GetPath(currentVertex, r)
+		cost = cost + cst
+		currentVertex = r
+	}
+	//dodaje drogę spowrotem do punktu startowego
+	cst, _ := ra.graph.GetPath(currentVertex, 0)
+	cost = cost + cst
+
+	return cost, path
 }
